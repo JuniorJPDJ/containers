b14e94805e4405c8e1eac1e430bb212c654c7490
JuniorJPDJ <github.com@juniorjpdj.pl>
2025-04-21 13:40:11 +0200

    fix(ejabberd-captcha): use correct captcha-ng.sh path on new containers

--- containers/ejabberd-captcha/custom_entrypoint.sh
+++ containers/ejabberd-captcha/custom_entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-export CAPTCHA_CMD="$(echo /home/ejabberd/lib/ejabberd-*/priv/bin/captcha-ng.sh)"
 export EJABBERD_CONFIG_PATH="/tmp/ejabberd.yml"
 
 envsubst < /home/ejabberd/conf/ejabberd.yml > $EJABBERD_CONFIG_PATH
